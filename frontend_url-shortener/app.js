6611aba03cb5f669667fcaa20859acda29e49171
Jonathan Andreas Pangkey <jonathanpangkey912@gmail.com>
2024-05-30 20:33:35 +0800

    add custom alias

--- frontend_url-shortener/app.js
+++ frontend_url-shortener/app.js
@@ -1,21 +1,22 @@
 document.getElementById("shorten-form").addEventListener("submit", async function (event) {
-    event.preventDefault();
-    const originalUrl = document.getElementById("originalUrl").value;
+  event.preventDefault();
+  const originalUrl = document.getElementById("originalUrl").value;
+  const customAlias = document.getElementById("customAlias").value;
 
-    const response = await fetch("http://localhost:8080/api/shorten", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({originalUrl}),
-    });
-
-    if (response.ok) {
-      const shortUrl = await response.text();
-      document.getElementById(
-        "short-url"
-      ).innerHTML = `Short URL: <a href="http://localhost:8080/api/${shortUrl}">http://localhost:8080/api/${shortUrl}</a>`;
-    } else {
-      document.getElementById("short-url").innerText = "Error shortening URL.";
-    }
+  const response = await fetch("http://localhost:8080/api/shorten", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({originalUrl, customAlias}),
   });
+
+  if (response.ok) {
+    const shortUrl = await response.text();
+    document.getElementById(
+      "short-url"
+    ).innerHTML = `Short URL: <a href="http://localhost:8080/api/${shortUrl}">http://localhost:8080/api/${shortUrl}</a>`;
+  } else {
+    document.getElementById("short-url").innerText = "Error shortening URL.";
+  }
+});
